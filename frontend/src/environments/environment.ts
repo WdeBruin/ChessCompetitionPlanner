487c27bb65792133dda5328b301e6790d801d9e6
WdeBruin <wouterdebruin88@gmail.com>
2018-08-17 15:25:22 +0200

    some changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  api: 'http://localhost:5000/api'
+  production: true,
+  api: 'http://https://chesscompetitionapi20180817031500.azurewebsites.net/api'
 };
 
 /*
