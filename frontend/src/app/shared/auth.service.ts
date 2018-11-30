092096a3113439e83b2cf8f56bcf5b7856adef11
Wouter de Bruin <wouter.de.bruin@capgemini.com>
2018-11-30 18:56:14 +0100

    Moved loggedIn and admin

--- frontend/src/app/shared/auth.service.ts
+++ frontend/src/app/shared/auth.service.ts
@@ -1,28 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
 import { User } from './user.model';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { take, tap } from 'rxjs/operators';
+import { take, tap, map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
-  user: BehaviorSubject<User> = new BehaviorSubject(null)
-  userDetails: User;
+  public user$: BehaviorSubject<User> = new BehaviorSubject(null)
+  public loggedIn: boolean;
+  public isAdmin: boolean;
 
   constructor(private _firebaseAuth: AngularFireAuth, private router: Router, private db: AngularFireDatabase) {
-    this.user.subscribe(
-      (user) => {
-        if (user) {
-          this.userDetails = user;
-        }
-        else {
-          this.userDetails = null;
-        }
-      }
-    );
+    this.user$.subscribe((user) => {
+      this.isAdmin = user !== null && user.roles !== null && user.roles.admin === true;
+      this.loggedIn = user !== null && user !== undefined;
+    });
   }
 
   signInWithGoogle() {
@@ -34,27 +29,10 @@ export class AuthService {
     })
   }
 
-  isLoggedIn() {
-    if (this.userDetails === null) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  isAdmin() {
-    if (this.userDetails === null || this.userDetails.roles.admin === null || this.userDetails.roles.admin === false) {
-      return false;
-    }
-    if (this.userDetails.roles.admin === true) {
-      return true;
-    }    
-  }
-
   logout() {
     this._firebaseAuth.auth.signOut()
       .then(() => {
-        this.user.next(null);
+        this.user$.next(null);
         this.router.navigate(['/']);
       });
   }
@@ -63,11 +41,11 @@ export class AuthService {
     const userData = new User(authData);
     const ref = this.db.object<User>(`users/${authData.user.uid}`);
     ref.valueChanges().pipe(
-      tap(user => {   
-        this.user.next(user);     
-        if(user === null) {
+      tap(user => {
+        this.user$.next(user);
+        if (user === null) {
           ref.update(userData);
-          this.user.next(userData);
+          this.user$.next(userData);
         }
       })
     ).subscribe();
--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,33 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AuthService } from '../shared';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
   @Input()
   public name: string;
 
-  public loggedIn$: Observable<boolean>;
-
-  constructor(private authService: AuthService) { }
-
-  ngOnInit() {
-    this.loggedIn$ = this.authService.user.pipe(
-      tap(u => console.log(u)),
-      map(u => u !== undefined && u !== null)
-      )
-  }
+  constructor(public authService: AuthService) { }
 
   logout() {
     this.authService.logout();
-  }
-
-  isAdmin() {
-    return this.authService.isAdmin();
   }
 }
