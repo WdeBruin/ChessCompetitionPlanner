04f28db2d4497aa875f5f4f645a6a3162cd2be96
Wouter <wouterdebruin88@gmail.com>
2019-06-21 23:51:04 +0200

    some naming

--- frontend/src/app/store/competition/competition.reducer.ts
+++ frontend/src/app/store/competition/competition.reducer.ts
@@ -1,54 +1,54 @@
+import { CompetitionState, Competition } from './competition.interface';
 import { createFeatureSelector } from '@ngrx/store';
 import { Status } from '../../shared';
-import { CompetitionState } from './';
-import * as actions from './competition.actions';
+import * as competitionActions from './competition.actions';
 
 export function CompetitionReducer(
-    state: CompetitionState = { data: [], status: undefined },
-    action: actions.CompetitionActions): CompetitionState {
-    switch (action.type) {
-        case actions.GET_COMPETITIONS:
-        case actions.UPDATE_COMPETITION:
-            return {
-                ...state,
-                status: Status.Loading
-            };
-        case actions.COMPETITION_ERROR:
-            return {
-                ...state,
-                status: Status.Error
-            };
-        case actions.GET_COMPETITIONS_SUCCESS:
-            action.competition.key = action.key;
-            if (state.data.find(x => x.key === action.key)) {
-                return {
-                    data: state.data.map(competition => {
-                        if (competition.key !== action.key) {
-                            return competition;
-                        }
-                        return action.competition;
-                    }),
-                    status: Status.Loaded
-                };
-            } else {
-                return {
-                    data: [...state.data, action.competition],
-                    status: Status.Loaded
-                };
+  state: CompetitionState = { data: [] as Competition[], status: undefined },
+  action: competitionActions.CompetitionActions): CompetitionState {
+  switch (action.type) {
+    case competitionActions.GET_COMPETITIONS:
+    case competitionActions.UPDATE_COMPETITION:
+      return {
+        ...state,
+        status: Status.Loading
+      };
+    case competitionActions.COMPETITION_ERROR:
+      return {
+        ...state,
+        status: Status.Error
+      };
+    case competitionActions.GET_COMPETITIONS_SUCCESS:
+      action.competition.key = action.key;
+      if (state.data.find(x => x.key === action.key)) {
+        return {
+          data: state.data.map(competition => {
+            if (competition.key !== action.key) {
+              return competition;
             }
-        case actions.UPDATE_COMPETITION_SUCCESS:
-            return {
-                status: Status.Loaded,
-                data: state.data.map(competition => {
-                    if (competition.key !== action.updatedCompetition.key) {
-                        return competition;
-                    }
-                    return action.updatedCompetition;
-                })
-            };
-        default:
-            return state;
-    }
+            return action.competition;
+          }),
+          status: Status.Loaded
+        };
+      } else {
+        return {
+          data: [...state.data, action.competition],
+          status: Status.Loaded
+        };
+      }
+    case competitionActions.UPDATE_COMPETITION_SUCCESS:
+      return {
+        status: Status.Loaded,
+        data: state.data.map(competition => {
+          if (competition.key !== action.updatedCompetition.key) {
+            return competition;
+          }
+          return action.updatedCompetition;
+        })
+      };
+    default:
+      return state;
+  }
 }
 
 export const competitionSelector = createFeatureSelector<CompetitionState>('competition');
